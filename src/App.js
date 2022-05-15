73d0e87378f8f8c9c27a065c2ec3f7ad60b6a783
turdaliyevjoraqozi <turdaliyevjoraqozi@gmail.com>
2022-05-15 09:56:56 +0500

    started super project for basco

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import About from "./Components/About/About";
+import Card from "./Components/Card/Card";
+import Header from "./Components/Header/Header";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
